b741d23b92cd32060abdeb060012d63fedceba1f
michaelchen1225 <90543072+michaelchen1225@users.noreply.github.com>
2025-06-02 16:24:44 +0800

    update

---
+++ krew-plug.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define plugin list
+PLUGINS=(
+  iexec
+  image
+  krew
+  neat
+  ns
+  pod-lens
+  sick-pods
+  status
+  view-allocations
+)
+
+# check if krew is installed
+
+if ! command -v kubectl krew &> /dev/null
+then
+    echo "krew is not installed. ref: https://krew.sigs.k8s.io/docs/user-guide/setup/install/"
+    exit 1
+fi
+
+
+# install plugins
+for plugin in "${PLUGINS[@]}"; do
+  kubectl krew install $plugin
+done
